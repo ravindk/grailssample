d4cdb02fd6a967825bd3e49d290cc092d753188b
Ravindra Karanam <ravindra.karanam@gmail.com>
2015-09-10 16:36:18 +0530

    roind 7

--- grails-app/controllers/grailssample/HelloController.groovy
+++ grails-app/controllers/grailssample/HelloController.groovy
@@ -3,6 +3,6 @@ package grailssample
 class HelloController {
 
     def index() {
-       render "Hello GtHub with Jenkins with CI / CD Polling attempt 3 waith war file update" 
+       render "Hello GtHub with Jenkins with CI / CD Polling last attempt hope this works 3 waith war file update" 
  }
 }
